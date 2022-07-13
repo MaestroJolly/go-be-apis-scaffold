21889473b17a3c7656488491ecaa123eaa6df9bb
MastroJolly <jolaosoyusuf@gmail.com>
2022-07-13 20:48:50 +0100

    added another test endpoints

--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
 	"log"
 	"net/http"
 
+	"pkgcentral.dev/go-be-apis-scaffold/src/routes"
 	"pkgcentral.dev/go-be-apis-scaffold/src/services/greetings"
 )
 
 func handleRequests() {
+	homePage := routes.HomePage
+	urlPath := routes.UrlPath
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/path/", urlPath)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
@@ -19,7 +23,7 @@ func main() {
 	handleRequests()
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
+// func homePage(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Welcome to the HomePage!")
+// 	fmt.Println("Endpoint Hit: homePage")
+// }
--- src/routes/home.go
+++ src/routes/home.go
@@ -5,7 +5,14 @@ import (
 	"net/http"
 )
 
+// This function returns the homepage data
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
+}
+
+// This function returns the URL query params
+
+func UrlPath(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the url query %q", r.URL.Path)
 }
